66dbaaee652463d6107659d74da180306dcf5352
lz <543972930@qq.com>
2021-10-18 15:45:02 +0800

    update coding

---
+++ testcase_api/test_login.py
@@ -0,0 +1,21 @@
+# -*- coding:utf-8 -*-
+# Author:lz
+
+import pytest
+
+
+class Testlogin:
+
+    @pytest.mark.run(order=1)
+    def test_01_login(self):
+        print("第一条用例")
+
+    @pytest.mark.run(order=3)
+    @pytest.mark.smoke
+    def test_03_login(self):
+        print("第三条用例")
+
+    @pytest.mark.run(order=2)
+    @pytest.mark.username
+    def test_04_login(self):
+        print("第四条用例")
---
+++ testcase_api/test_out.py
@@ -0,0 +1,12 @@
+# -*- coding:utf-8 -*-
+# Author:lz
+
+import pytest
+import time
+
+
+class TestOut:
+
+    def test_02_out(self):
+        time.sleep(3)
+        print("第二条用例")
